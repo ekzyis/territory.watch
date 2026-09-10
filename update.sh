3462fa4a9fe6d46591d23c74bd82e9e966244e9f
ekzyis <ramdip.singhgill@gmail.com>
2026-09-10 23:12:10 +0200

    systemd: fix 'permission denied (pubkey)' from git@github.com

--- update.sh
+++ update.sh
@@ -39,7 +39,7 @@ took=$((end-start))
 git commit -n --allow-empty \
   -m $'systemd: automated update\n\n' \
   -m "took $took seconds"
-git push
+GIT_SSH_COMMAND="ssh -i /home/ekzyis/.ssh/id_ed25519" git push
 
 # deploy new data
 #
